872713bff567633d2d0c7071715787838d09fc75
kardioversion1 <evan.kuhl@gmail.com>
2025-08-29 14:54:12 -0400

    refactor: convert server adapter to TS module

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/',
   resolve: { alias: { '@': resolve(__dirname, 'src') } },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        adapter: resolve(__dirname, 'src/server/adapter.ts'),
+      },
+    },
+  },
 });

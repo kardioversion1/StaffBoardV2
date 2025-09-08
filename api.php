b1f240d7531dd0b4608aac14d0fed0be31093f8d
kardioversion1 <evan.kuhl@gmail.com>
2025-09-07 23:12:47 -0400

    chore: remove deprecated Pragma headers

--- api.php
+++ api.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-header('Pragma: no-cache');
 
 $DATA_DIR = __DIR__ . '/data';
 if (!is_dir($DATA_DIR)) {
